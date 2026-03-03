428dfb223d2cb23de97fce9aacae0f974a06d34b
Parthvi-Kulkarni <43762449+Parthvi-Kulkarni@users.noreply.github.com>
2026-03-03 22:39:44 +0000

    Created react app template

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDe0q8_N3ntA1iAfSvyBj86uFRkCxqhf28",
+  authDomain: "clone-e5671.firebaseapp.com",
+  projectId: "clone-e5671",
+  storageBucket: "clone-e5671.firebasestorage.app",
+  messagingSenderId: "394853371144",
+  appId: "1:394853371144:web:2f6cff6afc4970ec53b0bd",
+  measurementId: "G-YXEQR3D223"
+};
